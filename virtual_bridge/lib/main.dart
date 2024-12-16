acca1d12031fe171cb08494ea3102e54e8de1e1d
neiz-choi <neiz.choi@kakaocorp.com>
2024-12-16 18:22:09 +0900

    tmp: 임시 저장

--- virtual_bridge/lib/main.dart
+++ virtual_bridge/lib/main.dart
@@ -1,12 +1,33 @@
-import 'package:digivice_virtual_bridge/nfc.dart';
+// import 'package:digivice_virtual_bridge/nfc.dart';
 import 'package:flutter/widgets.dart';
+// import 'package:nfc_manager/nfc_manager.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'dart:convert';
+// import 'dart:convert';
 
 String mapToString(Map<String, dynamic> map) {
   return map.entries
       .map((entry) => "[${entry.key}, ${entry.value}]")
       .join(", ");
+}
+
+class JavaScriptInterfaceArgsMapType {
+  final String id;
+  final Map<String, dynamic> args;
+
+  JavaScriptInterfaceArgsMapType({required this.id, required this.args});
+
+  factory JavaScriptInterfaceArgsMapType.fromMap(Map<String, dynamic> map) {
+    return JavaScriptInterfaceArgsMapType(
+      id: map['id'] as String,
+      args: map['args'] as Map<String, dynamic>,
+    );
+  }
+
+  @override
+  String toString() {
+    String serializedArgs = mapToString(args);
+    return '(id: $id, args: $serializedArgs)';
+  }
 }
 
 void main() {
@@ -22,18 +43,19 @@ class WebView extends StatelessWidget {
   WebView({super.key});
 
   final WebViewController _controller = WebViewController();
-  final NfcController _nfcController = NfcController();
+  // final NfcController _nfcController = NfcController();
 
   @override
   Widget build(BuildContext context) {
+    print('test');
     _controller
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..addJavaScriptChannel('__initJavascriptInterfaces',
-          onMessageReceived: _initJavascriptInterfaces)
-      ..addJavaScriptChannel('__native_nfcRead',
-          onMessageReceived: _handleNfcRead)
-      ..addJavaScriptChannel('__native_nfcWrite',
-          onMessageReceived: _handleNfcWrite)
+      // ..addJavaScriptChannel('__initJavascriptInterfaces',
+      //     onMessageReceived: _initJavascriptInterfaces)
+      // ..addJavaScriptChannel('__native_nfcRead',
+      //     onMessageReceived: _handleNfcRead)
+      // ..addJavaScriptChannel('__native_nfcWrite',
+      //     onMessageReceived: _handleNfcWrite)
       ..loadRequest(Uri.parse('http://172.20.37.209:5173'));
 
     return WebViewWidget(
@@ -42,58 +64,87 @@ class WebView extends StatelessWidget {
   }
 
   /// 'message.message' => args json 객체
-  /// ex) {option1: 123, option: 456, ...}
-  void _handleNfcRead(JavaScriptMessage message) {
-    Map<String, dynamic> args = jsonDecode(message.message);
-    var serializedMessage = message.message;
-    var serializedArgs = mapToString(args);
-    _log('NFC Read: "$serializedMessage"/$serializedArgs');
-  }
+  /// ex) {id, option1: 123, option: 456, ...}
+  // void _handleNfcRead(JavaScriptMessage message) {
+  //   JavaScriptInterfaceArgsMapType jsArgs =
+  //       jsonDecode(message.message) as JavaScriptInterfaceArgsMapType;
+  //   String serializedMessage = message.message;
+  //   String serializedJsArgs = jsArgs.toString();
+  //   _log('NFC Read: "$serializedMessage"/$serializedJsArgs');
+  //   // _nfcController.startReading(
+  //   //     onRead: ({required NdefMessage readNdefMessage}) {
+  //   //   _resolvePromise(
+  //   //       id: jsArgs.id, result: readNdefMessage.records.toString());
+  //   // });
+  // }
 
-  void _handleNfcWrite(JavaScriptMessage message) {
-    Map<String, dynamic> args = jsonDecode(message.message);
-    var serializedMessage = message.message;
-    var serializedArgs = mapToString(args);
-    _log('NFC Write: "$serializedMessage"/$serializedArgs');
-  }
+  // void _handleNfcWrite(JavaScriptMessage message) {
+  //   JavaScriptInterfaceArgsMapType jsArgs =
+  //       jsonDecode(message.message) as JavaScriptInterfaceArgsMapType;
+  //   String serializedMessage = message.message;
+  //   String serializedJsArgs = jsArgs.toString();
+  //   _log('NFC Write1: "$serializedMessage"/$serializedJsArgs');
+  //   // _nfcController.startWriting(
+  //   //     message: '@Nfc test message@',
+  //   //     onWritten: ({required NdefMessage writtenNdefMessage}) {
+  //   //       String serializedRecords = writtenNdefMessage.records.toString();
+  //   //       _log('NFC Write2: $serializedRecords');
+  //   //       _resolvePromise(id: jsArgs.id);
+  //   //     });
+  // }
 
-  /// apps/client/src/global.d.ts의 window 타입과 싱크
-  void _initJavascriptInterfaces(JavaScriptMessage message) {
-    _controller.runJavaScript('''
-      window.__generateId = () => {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-          var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-          return v.toString(16);
-        });
-      };
-      window.__promises = {};
-      window.__resolvePromise = (promiseId, data, error) => {
-        if (error) {
-          __promises[uuid].reject(error);
-        } else {
-          __promises[uuid].resolve(data);
-        }
-        delete __promises[uuid];
-      };
-      window.nfcController = {
-        readMessage: (argObj) => {
-          // TODO: promise id 생성 및 주입
-          __native_nfcRead.postMessage(JSON.stringify(argObj));
-        },
-        writeMessage: (argObj) => {
-          // TODO: promise id 생성 및 주입
-          __native_nfcWrite.postMessage(JSON.stringify(argObj));
-        }
-      }
-      console.log('Javascript interfaces was initialized.');
-    ''');
-  }
+  // /// apps/client/src/global.d.ts의 window 타입과 싱크
+  // void _initJavascriptInterfaces(JavaScriptMessage message) {
+  //   _controller.runJavaScript('''
+  //     window.__generateId = () => {
+  //       return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  //         const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+  //         return v.toString(16);
+  //       });
+  //     };
+  //     window.__promises = {};
+  //     window.__resolvePromise = (promiseId, result, error) => {
+  //       if (error) {
+  //         __promises[uuid].reject(error);
+  //       } else {
+  //         __promises[uuid].resolve(result);
+  //       }
+  //       delete __promises[uuid];
+  //     };
+  //     window.__createPromise = (cb) => {
+  //       const promise = new Promise((resolve, reject) => {
+  //         let id = __generateId();
+  //         if (__promises[id]) {
+  //           id = __generateId();
+  //         }
+  //         __promises[id] = { resolve, reject };
+  //         try {
+  //           cb();
+  //         } catch(exception) {
+  //           console.warn(exception);
+  //         }
+  //       });
+  //       return promise;
+  //     }
+  //     window.nfcController = {
+  //       readMessage: (argObj) => {
+  //         const serializedArgObj = JSON.stringify(argObj);
+  //         return __createPromise(() => __native_nfcRead.postMessage(serializedArgObj));
+  //       },
+  //       writeMessage: (argObj) => {
+  //         const serializedArgObj = JSON.stringify(argObj);
+  //         return __createPromise(() => __native_nfcWrite.postMessage(serializedArgObj));
+  //       }
+  //     }
+  //     console.log('Javascript interfaces was initialized.');
+  //   ''');
+  // }
 
-  Future<void> _log(String text) async {
-    await _controller.runJavaScript('console.log(`$text`)');
-  }
+  // Future<void> _log(String text) async {
+  //   await _controller.runJavaScript('console.log(`$text`)');
+  // }
 
-  // Future<void> _recordResponse(String response) async {
-  //   await _controller.runJavaScript('window.flutterResponse("$response")');
+  // void _resolvePromise({required String id, String? result}) {
+  //   _controller.runJavaScript('__resolvePromise(`$id`, `$result`)');
   // }
 }
--- virtual_bridge/lib/nfc.dart
+++ virtual_bridge/lib/nfc.dart
@@ -1,41 +1,56 @@
 import 'package:nfc_manager/nfc_manager.dart';
 
+// typedef NdefReadCallback = void Function(
+//     {required NdefMessage readNdefMessage});
+// typedef NdefWrittenCallback = void Function(
+//     {required NdefMessage writtenNdefMessage});
+
 class NfcController {
-  Map<String, dynamic> _latestReceived = {};
+  // Map<String, dynamic> _latestReceived = {};
 
-  void startReading() async {
-    try {
-      await NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-        _latestReceived = tag.data;
-        NfcManager.instance.stopSession();
-      });
-    } catch (e) {
-      // TODO:
-    }
-  }
+  // void startReading({required NdefReadCallback onRead}) async {
+  //   try {
+  //     await NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+  //       // _latestReceived = tag.data;
 
-  void startWriting(String message) async {
-    try {
-      await NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-        final ndef = Ndef.from(tag);
-        if (ndef == null || !ndef.isWritable) {
-          NfcManager.instance.stopSession(errorMessage: 'Tag not writable');
-          return;
-        }
+  //       final ndef = Ndef.from(tag);
+  //       if (ndef == null) {
+  //         NfcManager.instance.stopSession(errorMessage: 'Tag is null');
+  //         return;
+  //       }
 
-        final ndefMessage = NdefMessage([
-          NdefRecord.createText(message),
-        ]);
+  //       onRead(readNdefMessage: ndef.cachedMessage!);
+  //       NfcManager.instance.stopSession();
+  //     });
+  //   } catch (e) {
+  //     // TODO:
+  //   }
+  // }
 
-        try {
-          await ndef.write(ndefMessage);
-          NfcManager.instance.stopSession();
-        } catch (e) {
-          NfcManager.instance.stopSession(errorMessage: 'Write failed');
-        }
-      });
-    } catch (e) {
-      // TODO:
-    }
-  }
+  // void startWriting(
+  //     {required String message, NdefWrittenCallback? onWritten}) async {
+  //   try {
+  //     await NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+  //       final ndef = Ndef.from(tag);
+  //       if (ndef == null || !ndef.isWritable) {
+  //         NfcManager.instance.stopSession(errorMessage: 'Tag not writable');
+  //         return;
+  //       }
+
+  //       final ndefMessage = NdefMessage([
+  //         NdefRecord.createText(message),
+  //       ]);
+
+  //       try {
+  //         await ndef.write(ndefMessage);
+  //         onWritten!(writtenNdefMessage: ndefMessage);
+  //         NfcManager.instance.stopSession();
+  //       } catch (e) {
+  //         NfcManager.instance.stopSession(errorMessage: 'Write failed');
+  //       }
+  //     });
+  //   } catch (e) {
+  //     // TODO:
+  //   }
+  // }
 }
