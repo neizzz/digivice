0d4dede534a12305aad8c8f20058a85db460a74c
neiz-choi <neiz.choi@kakaocorp.com>
2024-12-20 19:24:08 +0900

    feat: android hce 세팅 (필요는 없으나, 혹시 몰라서 남겨둠)

---
+++ virtual_bridge/lib/nfc_hce.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter_nfc_hce/flutter_nfc_hce.dart';
+
+class NfcHceWritter {
+  // plugin instance
+  final _flutterNfcHcePlugin = FlutterNfcHce();
+
+  Future<String?> startWriting(String message) async {
+    String? platformVersion = await _flutterNfcHcePlugin.getPlatformVersion();
+    await _flutterNfcHcePlugin.isNfcHceSupported();
+    bool isSecureNfcEnabled = await _flutterNfcHcePlugin.isSecureNfcEnabled();
+    bool isNfcEnabled = await _flutterNfcHcePlugin.isNfcEnabled();
+
+    print(
+        '[NfcHceWritter::startWriting] $platformVersion, $isSecureNfcEnabled, $isNfcEnabled');
+
+    String? result =
+        await _flutterNfcHcePlugin.startNfcHce(message, persistMessage: false);
+
+    print('[NfcHceWritter::startWriting] result: $result');
+
+    return result;
+  }
+
+  void stopWriting() async {
+    await _flutterNfcHcePlugin.stopNfcHce();
+  }
+}
