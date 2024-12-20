2b9589a03189fa14ce0ea5868b2237ab4b41b187
neiz-choi <neiz.choi@kakaocorp.com>
2024-12-20 20:40:56 +0900

    chore: main.dart 코드정리

--- virtual_bridge/lib/main.dart
+++ virtual_bridge/lib/main.dart
@@ -1,8 +1,10 @@
-// import 'package:digivice_virtual_bridge/nfc.dart';
+// ignore_for_file: avoid_print
+
+import 'package:digivice_virtual_bridge/nfc_hce.dart';
+import 'package:digivice_virtual_bridge/nfc.dart';
 import 'package:flutter/widgets.dart';
-// import 'package:nfc_manager/nfc_manager.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-// import 'dart:convert';
+import 'dart:convert';
 
 String mapToString(Map<String, dynamic> map) {
   return map.entries
@@ -16,10 +18,14 @@ class JavaScriptInterfaceArgsMapType {
 
   JavaScriptInterfaceArgsMapType({required this.id, required this.args});
 
-  factory JavaScriptInterfaceArgsMapType.fromMap(Map<String, dynamic> map) {
+  Map<String, dynamic> toJson() {
+    return {'name': id, 'args': args};
+  }
+
+  factory JavaScriptInterfaceArgsMapType.fromJson(Map<String, dynamic> json) {
     return JavaScriptInterfaceArgsMapType(
-      id: map['id'] as String,
-      args: map['args'] as Map<String, dynamic>,
+      id: json['id'] as String,
+      args: json['args'] as Map<String, dynamic>,
     );
   }
 
@@ -39,23 +45,27 @@ void main() {
   );
 }
 
+// ignore: must_be_immutable
 class WebView extends StatelessWidget {
-  WebView({super.key});
-
   final WebViewController _controller = WebViewController();
-  // final NfcController _nfcController = NfcController();
+  late NfcController _nfcController;
+
+  WebView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    print('test');
+    _nfcController = NfcController();
     _controller
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      // ..addJavaScriptChannel('__initJavascriptInterfaces',
-      //     onMessageReceived: _initJavascriptInterfaces)
-      // ..addJavaScriptChannel('__native_nfcRead',
-      //     onMessageReceived: _handleNfcRead)
-      // ..addJavaScriptChannel('__native_nfcWrite',
-      //     onMessageReceived: _handleNfcWrite)
+      ..setOnConsoleMessage((JavaScriptConsoleMessage consoleMessage) {
+        print(consoleMessage.message);
+      })
+      ..addJavaScriptChannel('__initJavascriptInterfaces',
+          onMessageReceived: _initJavascriptInterfaces)
+      ..addJavaScriptChannel('__native_nfcRead',
+          onMessageReceived: _handleNfcRead)
+      ..addJavaScriptChannel('__native_nfcWrite',
+          onMessageReceived: _handleNfcWrite)
       ..loadRequest(Uri.parse('http://172.20.37.209:5173'));
 
     return WebViewWidget(
@@ -65,86 +75,101 @@ class WebView extends StatelessWidget {
 
   /// 'message.message' => args json 객체
   /// ex) {id, option1: 123, option: 456, ...}
-  // void _handleNfcRead(JavaScriptMessage message) {
-  //   JavaScriptInterfaceArgsMapType jsArgs =
-  //       jsonDecode(message.message) as JavaScriptInterfaceArgsMapType;
-  //   String serializedMessage = message.message;
-  //   String serializedJsArgs = jsArgs.toString();
-  //   _log('NFC Read: "$serializedMessage"/$serializedJsArgs');
-  //   // _nfcController.startReading(
-  //   //     onRead: ({required NdefMessage readNdefMessage}) {
-  //   //   _resolvePromise(
-  //   //       id: jsArgs.id, result: readNdefMessage.records.toString());
-  //   // });
-  // }
+  void _handleNfcRead(JavaScriptMessage message) async {
+    await _log(message.message);
+    var jsArgs = jsonDecode(message.message);
+    String serializedMessage = message.message;
+    await _log('_handleNfcRead raw message: "$serializedMessage"');
+    _nfcController.startReading(onRead: ({required String readMessage}) {
+      _resolvePromise(id: jsArgs['id'], data: readMessage);
+    });
+  }
 
-  // void _handleNfcWrite(JavaScriptMessage message) {
-  //   JavaScriptInterfaceArgsMapType jsArgs =
-  //       jsonDecode(message.message) as JavaScriptInterfaceArgsMapType;
-  //   String serializedMessage = message.message;
-  //   String serializedJsArgs = jsArgs.toString();
-  //   _log('NFC Write1: "$serializedMessage"/$serializedJsArgs');
-  //   // _nfcController.startWriting(
-  //   //     message: '@Nfc test message@',
-  //   //     onWritten: ({required NdefMessage writtenNdefMessage}) {
-  //   //       String serializedRecords = writtenNdefMessage.records.toString();
-  //   //       _log('NFC Write2: $serializedRecords');
-  //   //       _resolvePromise(id: jsArgs.id);
-  //   //     });
-  // }
+  void _handleNfcWrite(JavaScriptMessage message) async {
+    Map<String, dynamic> jsArgs = jsonDecode(message.message);
+    String serializedMessage = message.message;
+    String serializedJsArgs = jsArgs.toString();
+    await _log(
+        '_handleNfcWrite message: "$serializedMessage"/$serializedJsArgs');
+    _nfcController.startWriting(
+        message: '@Nfc test message@',
+        onWritten: ({required String writtenMessage}) {
+          print('onWritten called');
+          // String serializedRecords = writtenNdefMessage.records.toString();
+          _log('NFC Write2: $writtenMessage');
+          _resolvePromise(
+              id: (jsArgs as JavaScriptInterfaceArgsMapType).id,
+              data: writtenMessage);
+        });
+  }
 
-  // /// apps/client/src/global.d.ts의 window 타입과 싱크
-  // void _initJavascriptInterfaces(JavaScriptMessage message) {
-  //   _controller.runJavaScript('''
-  //     window.__generateId = () => {
-  //       return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-  //         const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-  //         return v.toString(16);
-  //       });
-  //     };
-  //     window.__promises = {};
-  //     window.__resolvePromise = (promiseId, result, error) => {
-  //       if (error) {
-  //         __promises[uuid].reject(error);
-  //       } else {
-  //         __promises[uuid].resolve(result);
-  //       }
-  //       delete __promises[uuid];
-  //     };
-  //     window.__createPromise = (cb) => {
-  //       const promise = new Promise((resolve, reject) => {
-  //         let id = __generateId();
-  //         if (__promises[id]) {
-  //           id = __generateId();
-  //         }
-  //         __promises[id] = { resolve, reject };
-  //         try {
-  //           cb();
-  //         } catch(exception) {
-  //           console.warn(exception);
-  //         }
-  //       });
-  //       return promise;
-  //     }
-  //     window.nfcController = {
-  //       readMessage: (argObj) => {
-  //         const serializedArgObj = JSON.stringify(argObj);
-  //         return __createPromise(() => __native_nfcRead.postMessage(serializedArgObj));
-  //       },
-  //       writeMessage: (argObj) => {
-  //         const serializedArgObj = JSON.stringify(argObj);
-  //         return __createPromise(() => __native_nfcWrite.postMessage(serializedArgObj));
-  //       }
-  //     }
-  //     console.log('Javascript interfaces was initialized.');
-  //   ''');
-  // }
+  /// apps/client/src/global.d.ts의 window 타입과 싱크
+  void _initJavascriptInterfaces(JavaScriptMessage message) {
+    _controller.runJavaScript('''
+      console.log(`Early errors: \${window.errorLogs}`);
+      window.__generateId = () => {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+          const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+          return v.toString(16);
+        });
+      };
+      window.__promises = {};
+      window.__resolvePromise = (promiseId, data, error) => {
+        if (error) {
+          __promises[promiseId].reject(error);
+        } else {
+          __promises[promiseId].resolve(data);
+        }
+        delete __promises[promiseId];
+      };
+      window.__createPromise = (cb) => {
+        const promise = new Promise((resolve, reject) => {
+          let id = __generateId();
+          if (__promises[id]) {
+            id = __generateId();
+          }
+          try {
+            cb(id);
+            __promises[id] = { resolve, reject };
+          } catch(exception) {
+            console.warn(exception);
+          }
+        });
+        return promise;
+      }
+      window.nfcController = {
+        readMessage: (rawArgObj = {}) => {
+          const promise = __createPromise((id) => {
+            const argObj = {
+              id,
+              args: rawArgObj
+            };
+            const serializedArgObj = JSON.stringify(argObj);
+            __native_nfcRead.postMessage(serializedArgObj);
+          });
+          return promise;
+        },
+        writeMessage: (rawArgObj) => {
+          const promise = __createPromise((id) => {
+            const argObj = {
+              id,
+              args: rawArgObj
+            };
+            const serializedArgObj = JSON.stringify(argObj);
+            __native_nfcWrite.postMessage(serializedArgObj);
+          });
+          return promise;
+        }
+      }
+      console.log('Javascript interfaces was initialized.');
+    ''');
+  }
 
-  // Future<void> _log(String text) async {
-  //   await _controller.runJavaScript('console.log(`$text`)');
-  // }
+  Future<void> _log(String text) async {
+    await _controller.runJavaScript('console.log(`$text`)');
+  }
 
-  // void _resolvePromise({required String id, String? result}) {
-  //   _controller.runJavaScript('__resolvePromise(`$id`, `$result`)');
-  // }
+  void _resolvePromise({required String id, String? data}) {
+    _controller.runJavaScript('__resolvePromise(`$id`, `$data`)');
+  }
 }
