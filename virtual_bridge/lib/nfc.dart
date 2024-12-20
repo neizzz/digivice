1febb4fa8a275566f2d8724557ded6ce8fabfc9d
neiz-choi <neiz.choi@kakaocorp.com>
2024-12-20 20:41:13 +0900

    refactor: model/record.dart 추가, workaround for blocking android new tag scanned popup

--- virtual_bridge/lib/nfc.dart
+++ virtual_bridge/lib/nfc.dart
@@ -1,56 +1,85 @@
+// ignore_for_file: avoid_print
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:android_intent_plus/android_intent.dart';
+import 'package:digivice_virtual_bridge/model/record.dart';
 import 'package:nfc_manager/nfc_manager.dart';
 
-// typedef NdefReadCallback = void Function(
-//     {required NdefMessage readNdefMessage});
-// typedef NdefWrittenCallback = void Function(
-//     {required NdefMessage writtenNdefMessage});
+typedef NdefReadCallback = void Function({required String readMessage});
+typedef NdefWrittenCallback = void Function({required String writtenMessage});
 
 class NfcController {
-  // Map<String, dynamic> _latestReceived = {};
+  NfcController();
 
-  // void startReading({required NdefReadCallback onRead}) async {
-  //   try {
-  //     await NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-  //       // _latestReceived = tag.data;
+  void startReading({required NdefReadCallback onRead}) async {
+    try {
+      bool isAvailable = await NfcManager.instance.isAvailable();
 
-  //       final ndef = Ndef.from(tag);
-  //       if (ndef == null) {
-  //         NfcManager.instance.stopSession(errorMessage: 'Tag is null');
-  //         return;
-  //       }
+      if (!isAvailable) {
+        if (Platform.isAndroid) {
+          //android 일 경우 nfc 설정창으로 이동 시킴
+          //ios는 nfc가 os단에서 감지하고 실행되기 때문에 안드로이드처럼 설정창으로 보낼 수 없다.
+          const AndroidIntent intent = AndroidIntent(
+            action: 'android.settings.NFC_SETTINGS',
+          );
+          await intent.launch();
+        }
+      }
 
-  //       onRead(readNdefMessage: ndef.cachedMessage!);
-  //       NfcManager.instance.stopSession();
-  //     });
-  //   } catch (e) {
-  //     // TODO:
-  //   }
-  // }
+      print('start session');
+      await NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+        final ndef = Ndef.from(tag);
+        if (ndef == null) {
+          NfcManager.instance.stopSession(errorMessage: 'Tag is null');
+          return;
+        }
 
-  // void startWriting(
-  //     {required String message, NdefWrittenCallback? onWritten}) async {
-  //   try {
-  //     await NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-  //       final ndef = Ndef.from(tag);
-  //       if (ndef == null || !ndef.isWritable) {
-  //         NfcManager.instance.stopSession(errorMessage: 'Tag not writable');
-  //         return;
-  //       }
+        WellknownTextRecord firstRecord =
+            WellknownTextRecord.fromNdefRecord(ndef.cachedMessage!.records[0]);
+        print('[NfcController::startReading] $firstRecord.toString()');
 
-  //       final ndefMessage = NdefMessage([
-  //         NdefRecord.createText(message),
-  //       ]);
+        onRead(readMessage: ndef.cachedMessage.toString());
 
-  //       try {
-  //         await ndef.write(ndefMessage);
-  //         onWritten!(writtenNdefMessage: ndefMessage);
-  //         NfcManager.instance.stopSession();
-  //       } catch (e) {
-  //         NfcManager.instance.stopSession(errorMessage: 'Write failed');
-  //       }
-  //     });
-  //   } catch (e) {
-  //     // TODO:
-  //   }
-  // }
+        // NOTE: [workaround]
+        // 바로 세션을 종료하게 되면, android에서 'New tag scanned' 팝업이 뜨게됨
+        // TODO: 좀 더 정교하게 통신할 수 있는 자체 프로토콜 정의 및 구현 필요
+        Timer(const Duration(seconds: 1), () {
+          NfcManager.instance.stopSession();
+          print('[NfcController::startReading] stop session');
+        });
+      });
+    } catch (e) {
+      // TODO:
+    }
+  }
+
+  void startWriting(
+      {required String message, NdefWrittenCallback? onWritten}) async {
+    try {
+      print('start session with message($message)');
+      await NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+        final ndef = Ndef.from(tag);
+        if (ndef == null || !ndef.isWritable) {
+          NfcManager.instance.stopSession(errorMessage: 'Tag not writable');
+          return;
+        }
+
+        final ndefMessage = NdefMessage([
+          NdefRecord.createText(message),
+        ]);
+
+        try {
+          await ndef.write(ndefMessage);
+          onWritten!(writtenMessage: message);
+          NfcManager.instance.stopSession();
+        } catch (e) {
+          NfcManager.instance.stopSession(errorMessage: 'Write failed');
+        }
+      });
+    } catch (e) {
+      // TODO:
+    }
+  }
 }
